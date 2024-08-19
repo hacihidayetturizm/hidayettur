5bcce95500b5968cc9ebc31dba7fb1fa8245bb0e
hacihidayetturizm <hacihidayetturizm@gmail.com>
2024-08-19 11:19:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,9 +6,18 @@ window.onload = function() {
         if (counter >= 100) {
             clearInterval(interval);
             setTimeout(function(){
-                document.querySelector('.loader').style.display = 'none';
-                document.querySelector('.content').style.opacity = '1';
-            }, 500); // Yüklenme animasyonu tamamlandıktan 0.5 saniye sonra kaybolacak
+                // Yüklenme ekranını yavaşça gizleyelim
+                document.querySelector('.loader').style.transition = 'opacity 0.5s ease';
+                document.querySelector('.loader').style.opacity = '0';
+
+                // Yüklenme ekranı tamamen kaybolduktan sonra içeriği gösterelim
+                setTimeout(function() {
+                    document.querySelector('.loader').style.display = 'none';
+                    document.querySelector('.content').style.display = 'block';
+                    document.querySelector('.content').style.transition = 'opacity 1s ease';
+                    document.querySelector('.content').style.opacity = '1';
+                }, 50); // Loader kaybolduktan 0.5 saniye sonra içerik yüklenir
+            }, 50); // Yüklenme animasyonu tamamlandıktan 0.5 saniye sonra kaybolacak
         }
     }, 40); // Sayacın 100'e kadar 4 saniyede ulaşması için 40ms aralıklarla güncelleniyor
 };
